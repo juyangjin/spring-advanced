8d9c868756d964d898b791c00e780795305b6e51
joyangjin <wndid2008@gmail.com>
2025-01-04 09:46:31 +0900

    :bulb:docs:add checkpoint

--- src/main/java/org/example/expert/client/WeatherClient.java
+++ src/main/java/org/example/expert/client/WeatherClient.java
@@ -27,6 +27,7 @@ public class WeatherClient {
                 restTemplate.getForEntity(buildWeatherApiUri(), WeatherDto[].class);
 
         WeatherDto[] weatherArray = responseEntity.getBody();
+        //else 문 정리할 필요 있어보임.
         if (!HttpStatus.OK.equals(responseEntity.getStatusCode())) {
             throw new ServerException("날씨 데이터를 가져오는데 실패했습니다. 상태 코드: " + responseEntity.getStatusCode());
         } else {
